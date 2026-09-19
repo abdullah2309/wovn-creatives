e6bcc1451c72edd9330bffad1c75e21ce68d0348
Abdullah <163624400+abdullah2309@users.noreply.github.com>
2026-09-19 09:40:33 +0500

    Move Footer component to MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-
 import styles from "./MainLayout.module.css";
 
 function MainLayout() {
@@ -13,7 +12,7 @@ function MainLayout() {
       <main className={styles.main}>
         <Outlet />
       </main>
-
+      
       <Footer />
     </div>
   );
