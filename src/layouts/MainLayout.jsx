4c87a3bea19f5ebc25032ea09ae7343dd555dcba
Abdullah <163624400+abdullah2309@users.noreply.github.com>
2026-09-23 11:26:47 +0500

    Remove unnecessary blank line in MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import styles from "./MainLayout.module.css";
